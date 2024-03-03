2c4f78d4d5572bab08d14c2b632f269adbe95494
wartakita <161806668+wartakita@users.noreply.github.com>
2024-03-03 23:08:52 +0700

    Update player.js

--- player.js
+++ player.js
@@ -1,48 +1,42 @@
-function playM3u8(url, license) {
-    var player = new Clappr.Player({
-        source: url,
-        parentId: '#player',
-        preload: 'auto',
-        autoPlay: 'true',
-        width: '100%',
-        height: '100%',
-        fullscreenEnabled: 'true',
-        hideMediaControl: 'false',
-        plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback],
+function playM3u8(url, license){                        
+ var player = new Clappr.Player({                 
+                    source: url,
+                    parentId: '#player',
+                    preload: 'auto',
+                    autoPlay: 'true',
+                    width: '100%',
+                    height: '100%',
+                    fullscreenEnabled:'true',
+                    hideMediaControl: 'false',
+                    plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback],
+           
+                    chromecast: {
+                      appId: '9DFB77C0',
+                      media: {
+                        type: ChromecastPlugin.Movie,
+                        title: 'Player HLS - DASH',
+                      },
+                  },
 
-        chromecast: {
-            appId: '9DFB77C0',
-            media: {
-                type: ChromecastPlugin.Movie,
-                title: 'Player HLS - DASH',
-            },
-        },
-
-        shakaConfiguration: {
-            preferredAudioLanguage: 'en-US',
-            drm: {
-                servers: {
+                    shakaConfiguration: {
+                    preferredAudioLanguage: 'en-US',
+                    drm: {
+                    servers: {
                     'com.widevine.alpha': license
-                }
-            },
-            streaming: {
-                rebufferingGoal: 15
-            }
-        },
-        shakaOnBeforeLoad: function(shaka_player) {
-            // shaka_player.getNetworkingEngine().registerRequestFilter() ...
-        },
-    });
+                             }
+                         },
+                    streaming: {
+                      rebufferingGoal: 15
+                    }
+                  },
+                  shakaOnBeforeLoad: function(shaka_player) {
+                    // shaka_player.getNetworkingEngine().registerRequestFilter() ...
+                  },
 
-    document.title = "Watching: " + url;
+                });
 
-    // Fungsi untuk mendapatkan URL saat ini dari video yang diputar
-    function getURL() {
-        return player.getCurrentSource();
-    }
-
-    // Menambahkan fungsi getURL ke dalam objek player agar bisa diakses dari luar
-    player.getURL = getURL;
+  document.title = "Watching: " +url;
+    
 }
 
-playM3u8(window.location.href.split("#")[1], window.location.href.split("=")[1]);
+playM3u8(window.location.href.split("#")[1], window.location.href.split("=")[1])
