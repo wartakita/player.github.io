1c701efeec28a588a5c315a98a7a49f6d2e51de4
wartakita <161806668+wartakita@users.noreply.github.com>
2024-06-16 08:02:51 +0700

    Create get.js

---
+++ get.js
@@ -0,0 +1,14 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\[").replace(/[\]]/, "\]");
+    var regex = new RegExp("[\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var getURL = getParameterByName('url');
+var getIMG = getParameterByName('img');
+var getKEY = getParameterByName('k1');
+var getKEY2 = getParameterByName('k2');
+if (getURL == "#") {
+    alert('Go back to the previous page');
+}
