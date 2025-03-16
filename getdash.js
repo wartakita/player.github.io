fc03f116e75f14d3969adfcee4edd5d5cc349139
wartakita <161806668+wartakita@users.noreply.github.com>
2025-03-16 11:53:02 +0700

    Create getdash.js

---
+++ getdash.js
@@ -0,0 +1,22 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\[").replace(/[\]]/, "\]");
+    var regex = new RegExp("[\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+var getURL = getParameterByName('url');
+var getIMG = getParameterByName('img');
+var getKEY = getParameterByName('k1');
+var getKEY2 = getParameterByName('k2');
+if (getURL == "#") {
+    alert('Go back to the previous page');
+} <
+/script> <
+script >
+    var play = getParameterByName('play');
+
+if (play == "1") {
+    play = "true"
+} else {
+    play = "false"
+}
