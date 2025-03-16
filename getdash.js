e2b23dee8ba59a12990ba9b70f14a5c9a6340e09
wartakita <161806668+wartakita@users.noreply.github.com>
2025-03-16 11:57:56 +0700

    Update getdash.js

--- getdash.js
+++ getdash.js
@@ -1,22 +1,11 @@
 function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\[").replace(/[\]]/, "\]");
-    var regex = new RegExp("[\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	name = name.replace(/[\[]/, "\[").replace(/[\]]/, "\]");
+	var regex = new RegExp("[\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 var getURL = getParameterByName('url');
 var getIMG = getParameterByName('img');
-var getKEY = getParameterByName('k1');
-var getKEY2 = getParameterByName('k2');
-if (getURL == "#") {
-    alert('Go back to the previous page');
-} <
-/script> <
-script >
-    var play = getParameterByName('play');
-
-if (play == "1") {
-    play = "true"
-} else {
-    play = "false"
-}
+var getKEY  = getParameterByName('k1');
+var getKEY2  = getParameterByName('k2');
+if (getURL == "#") {alert('Go back to the previous page');}
